7eb39d424af06f2a77a0deb50006d14205b10ca5
Eli Craig <elidcraig@gmail.com>
2022-04-25 15:56:17 -0700

    commit tree b99de65c7d3149d0f226d8ec311fcd7d0a00ab5c

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    let distanceInBlocks;
+    if (location >= 42) {
+        distanceInBlocks = location - 42;
+    } else if (location < 42) {
+        distanceInBlocks = 42 - location;
+    }
+    return distanceInBlocks;
+}
+
+function distanceFromHqInFeet(location) {
+    const distanceInBlocks = distanceFromHqInBlocks(location);
+    const distanceInFeet = distanceInBlocks * 264;
+    return distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distance;
+    if (start >= destination) {
+        distance = (start - destination) * 264;
+    } else if (start < destination) {
+        distance = (destination - start) * 264;
+    }
+    return distance;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare;
+    if (distance <= 400) {
+        fare = 0;
+    } else if (distance <= 2000) {
+        fare = (distance - 400) * 0.02;
+    } else if (distance <= 2500) {
+        fare = 25;
+    } else {
+        fare = 'cannot travel that far';
+    }
+    return fare;
+}
